0a717abb6dbe87c87e8d49520609c2ec77fdd4e5
Tosh-PC <lextor78@gmail.com>
2022-04-05 23:15:47 +0600

    2. Тестируем PostController

--- README.md
+++ README.md
@@ -4,11 +4,22 @@
 [![codecov](https://codecov.io/gh/aswsx/job4j_dreamjob/branch/master/graph/badge.svg?token=rIPqeJScWw)](https://codecov.io/gh/aswsx/job4j_threads)
 [![CircleCI](https://circleci.com/gh/aswsx/job4j_dreamjob/tree/master.svg?style=shield)](https://circleci.com/gh/aswsx/job4j_threads/tree/master)
 ![GitHub commit activity](https://img.shields.io/github/commit-activity/w/aswsx/job4j_dreamjob)
+![GitHub last commit](https://img.shields.io/github/last-commit/aswsx/job4j_dreamjob)
+![GitHub top language](https://img.shields.io/github/languages/top/aswsx/job4j_dreamjob)
+![GitHub language count](https://img.shields.io/github/languages/count/aswsx/job4j_dreamjob)
+![Lines of code](https://img.shields.io/tokei/lines/github/aswsx/job4j_dreamjob)
 
-## Используемые технологии
+## Навыки 
 
-<p align="center">
- <img src="Pictures/logo-java.png" height="100">
-<img src="Pictures/jacoco.png" height="60">
-<img src="Pictures/maven.png" height="50"> 
-</p>
+![Java](https://img.shields.io/badge/-Java-003f5c?style=for-the-badge&logo=Java)
+![Spring](https://img.shields.io/badge/-Spring-003f5c?style=for-the-badge&logo=Spring)
+![PostgreSQL](https://img.shields.io/badge/-PostgreSQL-003f5c?style=for-the-badge&logo=POstgreSQL)
+![Actions](https://img.shields.io/badge/-GithubActions-003f5c?style=for-the-badge&logo=GithubActions)
+![TravisCI](https://img.shields.io/badge/-TravisCI-003f5c?style=for-the-badge&logo=TravisCI)
+![CircleCI](https://img.shields.io/badge/-CircleCI-003f5c?style=for-the-badge&logo=CircleCI)
+![Codecov](https://img.shields.io/badge/-Codecov-003f5c?style=for-the-badge&logo=Codecov)
+![HTML](https://img.shields.io/badge/-HTML-003f5c?style=for-the-badge&logo=HTML)
+![JDBC](https://img.shields.io/badge/-JDBC-003f5c?style=for-the-badge&logo=JDBC)
+![Bootstrap](https://img.shields.io/badge/-Bootstrap-003f5c?style=for-the-badge&logo=Bootstrap)
+![ThymeLeaf](https://img.shields.io/badge/-ThymeLeaf-003f5c?style=for-the-badge&logo=ThymeLeaf)
+![Mockito](https://img.shields.io/badge/-Mockito-003f5c?style=for-the-badge&logo=Mockito)
---
+++ src/test/java/ru/job4j/dreamjob/controller/PostControllerTest.java
@@ -0,0 +1,68 @@
+package ru.job4j.dreamjob.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.ui.Model;
+import ru.job4j.dreamjob.control.PostController;
+import ru.job4j.dreamjob.model.Post;
+import ru.job4j.dreamjob.service.CityService;
+import ru.job4j.dreamjob.service.PostService;
+
+import javax.servlet.http.HttpSession;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.*;
+
+/**
+ * @author Alex Gutorov
+ * @version 1.0
+ * @created 04/04/2022 - 22:49
+ */
+class PostControllerTest {
+
+    @Test
+    void whenAddPost() {
+        HttpSession session = mock(HttpSession.class);
+        Model model = mock(Model.class);
+        PostService postService = mock(PostService.class);
+        CityService cityService = mock(CityService.class);
+
+        List<Post> posts = Arrays.asList(
+                new Post(1, "New post"),
+                new Post(2, "New post")
+        );
+
+        when(postService.findAll()).thenReturn(posts);
+        PostController postController = new PostController(
+                postService,
+                cityService
+        );
+        String page = postController.posts(model, session);
+
+        verify(model).addAttribute("posts", posts);
+        assertThat(page, is("posts"));
+    }
+
+    @Test
+    void whenUpdatePost() {
+        HttpSession session = mock(HttpSession.class);
+        Model model = mock(Model.class);
+        PostService postService = mock(PostService.class);
+        CityService cityService = mock(CityService.class);
+
+        Post post = new Post(1, "New post");
+        Post modPost = new Post(1, "Modified post");
+
+        when(postService.findById(1)).thenReturn(modPost);
+        PostController postController = new PostController(
+                postService,
+                cityService
+        );
+        String page = postController.formUpdatePost(model, 1, session);
+
+        verify(model).addAttribute("post", modPost);
+        assertThat(page, is("updatePost"));
+    }
+}
