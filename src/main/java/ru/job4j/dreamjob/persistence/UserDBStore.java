b13c502b75f7565c0aee10cdf04ef73aef133514
Tosh-PC <lextor78@gmail.com>
2022-04-05 23:46:12 +0600

    ref

--- src/main/java/ru/job4j/dreamjob/persistence/UserDBStore.java
+++ src/main/java/ru/job4j/dreamjob/persistence/UserDBStore.java
@@ -60,5 +60,25 @@ public class UserDBStore {
             e.printStackTrace();
         }
     }
+
+    public Optional<User> findUserByEmailAndPwd(String email, String password) {
+        try (Connection cn = pool.getConnection();
+             PreparedStatement ps = cn.prepareStatement("SELECT * FROM users WHERE id = ? and password = ?")
+        ) {
+            ps.setString(1, email);
+            ps.setString(2, password);
+            try (ResultSet it = ps.executeQuery()) {
+                if (it.next()) {
+                    return Optional.of(new User(it.getInt("id"),
+                            it.getString("name"),
+                            it.getString("email"),
+                            it.getString("password")));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Optional.empty();
+    }
 }
 
